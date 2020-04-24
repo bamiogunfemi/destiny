49e10faa01d95d76ecf18cb84be6e01b9298321a
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-04-24 18:33:11 +0100

    background reup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_particles/particles.dart';
+
 import 'story_brain.dart';
 
 void main() => runApp(Destini());
@@ -6,8 +8,8 @@ void main() => runApp(Destini());
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: StoryPage(),
+
+      home:  StoryPage()
     );
   }
 }
@@ -23,84 +25,91 @@ class _StoryPageState extends State<StoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-        color: const Color(0xff7c94b6),
-    image: const DecorationImage(
-    image: AssetImage('images/background.png'),
-    fit: BoxFit.cover,
-    ),),
-        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                flex: 12,
-                child: Center(
-                  child: Text(
-                    storyBrain.getStory(),
-                    style: TextStyle(
-                      fontSize: 25.0,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 1 made by user.
-                    setState(() {
-                      storyBrain.nextStory(1);
-                    });
-
-                  },
-                  color: Colors.red,
-                  child: Text(
-
-                    storyBrain.getChoice1(),
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Expanded(
-                flex: 2,
-
-                child: Visibility(
-                  visible: storyBrain.buttonShouldBeVisible(),
-                  child: FlatButton(
-                    onPressed: () {
-                      //Choice 2 made by user.
-                      setState(() {
-                        storyBrain.nextStory(2);
-                      });
-
-                    },
-                    color: Colors.blue,
+      body: Stack(
+children: <Widget>[
+  Particles(
+    30, // Number of Particles
+    Colors.purple, // Color of Particle
+  ),
+         SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  flex: 12,
+                  child: Center(
                     child: Text(
-
-                      storyBrain.getChoice2(),
+                      storyBrain.getStory(),
                       style: TextStyle(
-                        fontSize: 20.0,
+                        fontSize: 25.0,
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: FlatButton(
+                        onPressed: () {
+                          //Choice 1 made by user.
+                          setState(() {
+                            storyBrain.nextStory(1);
+                          });
+
+                        },
+                        color: Colors.purple,
+                        child: Text(
+
+                          storyBrain.getChoice1(),
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                SizedBox(
+                  width: 20.0,
+                ),
+                Expanded(
+                  flex: 2,
+
+                  child: Visibility(
+                    visible: storyBrain.buttonShouldBeVisible(),
+                    child: FlatButton(
+                      onPressed: () {
+                        //Choice 2 made by user.
+                        setState(() {
+                          storyBrain.nextStory(2);
+                        });
+
+                      },
+                      color: Colors.purple,
+                      child: Text(
+
+                        storyBrain.getChoice2(),
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+              ],
+
+            ),
+
         ),
-      ),
+         )
+]
+         ),
+
     );
   }
 }
-
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
